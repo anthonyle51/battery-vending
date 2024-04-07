aae1866a5cfdf6e6c6f7b179d92f6f4f53e3d6b6
anthonyle51 <anthonyle20045@gmail.com>
2024-04-07 17:42:30 -0400

    duracell picture

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Product = ({ product, onAddToCart }) => {
+    console.log(product.imageURL)
     return (
         <div className="product" onClick={() => onAddToCart(product)} style={{ cursor: 'pointer', margin: '10px', border: '1px solid #ddd', padding: '10px', borderRadius: '5px', textAlign: 'center' }}>
-            <img src={'src/assets/batteries/' + product.imageURL} alt={product.name} style={{ maxWidth: '100px', maxHeight: '100px' }}></img>
+            <img src={'public/images/batteries/' + product.imageURL} alt={product.imageURL} style={{ maxWidth: '100px', maxHeight: '100px' }}></img>
             <h2>{product.name}</h2>
             <p>${product.price.toFixed(2)}</p>
         </div>
@@ -12,4 +13,4 @@ const Product = ({ product, onAddToCart }) => {
 
 export default Product;
 
-// <img src={'../assets/batteries' + product.imageURL}></img>
+// <img src={'public/images/batteries/' + product.imageURL}></img>
--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -3,10 +3,11 @@ import Product from './Product';
 
 const Selection = ({ onAddToCart }) => {
     const products = [
-        { id: 1, name: 'Lithium AA', price: 5.99, imageUrl: 'energizer-lithium-AA.jpg' },
-        { id: 2, name: 'Lithium AAA', price: 6.99, imageUrl: 'energizer-lithium-AAA.webp' },
-        { id: 3, name: 'Alkaline AA', price: 5.99, imageUrl: 'duracell-alkaline-AA.jpg' },
-        { id: 4, name: 'Alkaline AAA', price: 6.99, imageUrl: 'energizer-alkaline-AAA.jpg' },
+        
+        { id: 1, name: 'Lithium AA', price: 5.99, imageURL: 'energizer-lithium-AA.jpg' },
+        { id: 2, name: 'Lithium AAA', price: 6.99, imageURL: 'energizer-lithium-AAA.webp' },
+        { id: 3, name: 'Alkaline AA', price: 5.99, imageURL: 'duracell-alkaline-AA.jpg' },
+        { id: 4, name: 'Alkaline AAA', price: 6.99, imageURL: 'energizer-alkaline-AAA.jpg' },
         
     ];
 
